d71e713ab30ec57d736cd753538a55b57d4a08df
maciej-misiak <maciej.misiak@microsoft.wsei.edu.pl>
2022-06-21 02:25:01 +0200

    dodawanie

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace WpfApp1
 {
@@ -23,6 +25,102 @@ namespace WpfApp1
         public MainWindow()
         {
             InitializeComponent();
+            ZaladujGrid();
+        }
+
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-8GAO498\SQLEXPRESS;Initial Catalog=NewDB;Integrated Security=True");
+
+        public void wyczyscDane()
+        {
+            nazwa_txt.Clear();
+            adres_txt.Clear();
+            nip_txt.Clear();
+            email_txt.Clear();
+        }
+
+        public bool isValid()
+        {
+            if (nazwa_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Nazwa jest pusta", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (adres_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Adres jest pusty", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (nip_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Nip jest pusty", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (email_txt.Text == string.Empty)
+            {
+                MessageBox.Show("Email jest pusty", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        public void ZaladujGrid()
+        {
+            SqlCommand cmd = new SqlCommand("select * from FirstTable", con);
+            DataTable dt = new DataTable();
+            con.Open();
+            SqlDataReader sdr = cmd.ExecuteReader();
+            dt.Load(sdr);
+            con.Close();
+            datagrid.ItemsSource = dt.DefaultView;
+
+        }
+private void DodajBtn_txt_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (isValid())
+                {
+                    SqlCommand cmd = new SqlCommand("INSERT INTO FirstTable VALUES (@Nazwa, @Adres, @Nip, @Email)", con);
+                    cmd.CommandType = System.Data.CommandType.Text;
+                    cmd.Parameters.AddWithValue("@Nazwa", nazwa_txt.Text);
+                    cmd.Parameters.AddWithValue("@Adres", adres_txt.Text);
+                    cmd.Parameters.AddWithValue("@Nip", nip_txt.Text);
+                    cmd.Parameters.AddWithValue("@Email", email_txt.Text);
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+                    ZaladujGrid();
+                    MessageBox.Show("Klienta dodano pomyślnie", "Zapisano", MessageBoxButton.OK, MessageBoxImage.Information);
+                    wyczyscDane();
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    
+    private void WyczyscBtn_txt_Click(object sender, RoutedEventArgs e)
+        {
+            wyczyscDane();
+        }
+
+        private void UsunBtn_txt_Click(object sender, RoutedEventArgs e)
+        {
+            con.Open();
+            SqlCommand cmd = new SqlCommand("delete from FirstTable where ID = "+szukaj_txt.Text+ " ", con);
+            try
+            {
+
+            }
+            catch (SqlException ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
